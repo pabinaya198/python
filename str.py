1fa9f62eaf6ef9285d9f965aacd2eb1fb0d7ecb4
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-08-04 17:00:07 +0530

    Create str.py

---
+++ str.py
@@ -0,0 +1,10 @@
+message=("happy birthday")
+print(len (message))
+print(message.find("b"))
+print(message.count("a"))
+print(message.replace("a","r"))
+print(message.isalpha())
+print(message.isdigit())
+
+message=" Happy Birthday Abi "
+print(message * 100)
