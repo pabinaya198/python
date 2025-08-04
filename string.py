38a752ba58bdb7d55f6fffac3291d7d63cf15a53
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-08-04 16:13:33 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,10 @@
+message="his son's book"
+name='RAM'
+quote='''peace'''
+print(quote)
+print(name.upper())
+print(name.lower())
+message="happy birthday"
+print(message.title())
+print(message + name)
+print("Hello \n World")
