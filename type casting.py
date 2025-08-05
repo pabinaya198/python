84c97bdc69a73a0856b56423a5f9151e4ff246f4
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-08-05 17:19:31 +0530

    Create type casting.py

---
+++ type casting.py
@@ -0,0 +1,11 @@
+otp="785421"
+otp=str(otp)
+print(" your otp is " + str(otp))
+print(type(otp))
+
+count="20"
+print(int(count) + 1)
+
+count="20.5"
+print(float(count) + 1)
+print(int(float(count)) + 1)
