86a54199e8ce24c2002f6eadf92d85796e094bb1
pabinaya198 <147413476+pabinaya198@users.noreply.github.com>
2025-08-22 16:25:04 +0530

    Create variable scope.py

---
+++ variable scope.py
@@ -0,0 +1,18 @@
+def welcome(name):
+    print("welcome" + name)
+welcome("radha")
+
+num=10
+while num<=100:
+    print(num)
+    num+=10
+
+def welcome(name):
+    num=20
+    print("welcome" + name)
+    print(str(num))
+welcome("radha")
+print("the value of num is" + str(num))
+while num<=100:
+    print(num)
+    num+=10
